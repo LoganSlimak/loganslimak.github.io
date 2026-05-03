2a4ad5af18dbf3c0037a6023afaa824bab413209
LoganSlimak <loganmslimak@gmail.com>
2026-05-03 09:57:07 -0400

    dropdown (:

--- dropdown.js
+++ dropdown.js
@@ -1,15 +1,26 @@
+function open(id) {
+    const allBodies = document.querySelectorAll(".body");
 
-    function open(id) {
-        const allBodies = document.querySelectorAll(".body");
+    allBodies.forEach(body => {
+        body.style.maxHeight = null;
+    }); // closes all
 
-        allBodies.forEach(body => {
-            body.style.maxHeight = null;
-        }); // closes all
+    const sect = document.getElementById(id);
+    if (!sect) return;
 
-        const sect = document.getElementById(id);
-        if (!sect) return;
+    const body = sect.querySelector(".body");
+    body.style.maxHeight = body.scrollHeight + "px";
+    sect.scrollIntoView({ behavior: "smooth" });
+}
+function toggle(header) {
+    const allBodies = document.querySelectorAll(".body");
 
-        const body = sect.querySelector(".body");
-        body.style.maxHeight = body.scrollHeight + "px";
-        sect.scrollIntoView({ behavior: "smooth" });
+    allBodies.forEach(body => {
+    if (body !== header.nextElementSibling) {
+        body.style.maxHeight = null;
     }
+    }); // closes all
+
+    const body = header.nextElementSibling;
+    body.style.maxHeight = body.style.maxHeight ? null : body.scrollHeight + "px";
+}
